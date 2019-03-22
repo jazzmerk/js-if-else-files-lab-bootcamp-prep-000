cef330a61bbe001e646932f3183e666ec4f2e007
James Merkin <jnmerk@gmail.com>
2019-03-22 18:30:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var name = "Susan"
+var name = "JOe"
 var height = "74"
 if (name==="Susan"){
   console.log("Correctomundo")
+}
+
+else{
+  console.log ("You Have failed")
 }
 
 // Don't worry about this
